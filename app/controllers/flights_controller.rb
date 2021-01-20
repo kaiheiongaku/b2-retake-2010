8ad9123c5253b4d5574011293f62d3ac0bb0c31b
Robert Heath <robert.b.heathii@gmail.com>
2021-01-20 12:35:10 -0600

    update controller with destroy action

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,4 +2,12 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
   end
+
+  def destroy
+    @passenger = Passenger.find(params[:passenger_id])
+    @flight = Flight.find(params[:flight_id])
+    @flight_passenger = FlightPassenger.find_by_data(@passenger.id, @flight.id)
+    @flight_passenger.destroy
+    redirect_to flights_path
+  end
 end
