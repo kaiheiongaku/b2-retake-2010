f6ddd38deddd9b01f5d9e92c20356897865a2f8d
Robert Heath <robert.b.heathii@gmail.com>
2021-01-20 12:48:25 -0600

    Convert instance variables to local variables

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,10 +4,10 @@ class FlightsController < ApplicationController
   end
 
   def destroy
-    @passenger = Passenger.find(params[:passenger_id])
-    @flight = Flight.find(params[:flight_id])
-    @flight_passenger = FlightPassenger.find_by_data(@passenger.id, @flight.id)
-    @flight_passenger.destroy
+    passenger = Passenger.find(params[:passenger_id])
+    flight = Flight.find(params[:flight_id])
+    flight_passenger = FlightPassenger.find_by_data(passenger.id, flight.id)
+    flight_passenger.destroy
     redirect_to flights_path
   end
 end
