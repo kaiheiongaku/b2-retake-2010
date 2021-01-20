f174af806d3f1c077d8e99281cb52733a6c99920
Robert Heath <robert.b.heathii@gmail.com>
2021-01-20 12:34:30 -0600

    add route for deleting

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :flights, only: [:index]
+
+  delete "/flights/:flight_id/passengers/:passenger_id", to: "flights#destroy"
+  resources :flights, only: [:index, :delete]
   resources :airlines, only: [:show]
 end
