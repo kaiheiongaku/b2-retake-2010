6eda312e187ede6add59ca7e1c4ffcb08a245172
Robert Heath <robert.b.heathii@gmail.com>
2021-01-20 11:43:44 -0600

    Code unique passengers for an airline

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,7 @@
 class Airline < ApplicationRecord
   has_many :flights
+
+  def unique_passengers
+    Passenger.joins(flights: :flight_passengers).where('flights.airline_id = ?', self.id).distinct
+  end
 end
