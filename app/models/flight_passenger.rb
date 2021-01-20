4edd570e933db0e240ebfb60d5ca244d1e30d94e
Robert Heath <robert.b.heathii@gmail.com>
2021-01-20 12:31:51 -0600

    add method to flight_passenger

--- app/models/flight_passenger.rb
+++ app/models/flight_passenger.rb
@@ -1,4 +1,8 @@
 class FlightPassenger < ApplicationRecord
   belongs_to :flight
   belongs_to :passenger
+
+  def self.find_by_data(passenger_id, flight_id)
+    where(passenger_id: passenger_id, flight_id: flight_id).first
+  end
 end
